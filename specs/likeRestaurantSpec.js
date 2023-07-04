399d4d6c9b583fd5e4a5580673d0b1248e60b48f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-07-04 11:26:39 +0800

    refactor test code

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -12,39 +12,32 @@ describe('Like a Restaurant', () => {
 
   it('should show the like widget when the restaurant has not been liked before', async () => {
     await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
-
     expect(document.getElementById('likeButton')).toBeTruthy();
   });
 
   it('should not show the unlike widget when the restaurant has not been liked before', async () => {
     await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
-
     expect(document.getElementById('unlikeButton')).toBeFalsy();
   });
 
   it('should be able to like the restaurant', async () => {
     await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
-
     document.getElementById('likeButton').dispatchEvent(new Event('click'));
     const restaurant = await FavoriteRestaurantIdb.getRestaurant(1);
     expect(restaurant).toEqual({ id: 1 });
-
     await FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
   it('should not add a restaurant again when its already liked', async () => {
     await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
-
     await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
     document.getElementById('likeButton').dispatchEvent(new Event('click'));
     expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([{ id: 1 }]);
-
     await FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
   it('should not add a restaurant when it has no id', async () => {
     await TestFactories.createLikeButtonPresenterWithRestaurant({});
-
     document.getElementById('likeButton').dispatchEvent(new Event('click'));
     expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
   });
